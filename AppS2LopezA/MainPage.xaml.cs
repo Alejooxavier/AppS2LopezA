ab8aaf39bcbe827b01a8b112f20efbfc16f17511
AlejandroLopez <65260293+Alejooxavier@users.noreply.github.com>
2021-10-19 19:52:12 -0500

    Revert "Revert "Agregar archivos de proyecto.""
    This reverts commit 292fd6d7ca7f6cc8e13e21ea735d4356575f28f9.

---
+++ AppS2LopezA/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppS2LopezA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //convertir el valor txt a dobule.
+                double valor1 = Convert.ToDouble(txtValorUno.Text);
+                double valor2 = Convert.ToDouble(txtValorDos.Text);
+
+                //operaciones.
+                double suma = valor1 + valor2;
+
+                //visualizar resultado en pantalla.
+                txtResultado.Text = suma.ToString();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", ex.Message, "OK");
+            }
+        }
+    }
+}
